d34c8e92c6e845ead1776a2487af19c66eed7cca
AlvaroCabral02 <88688654+AlvaroCabral02@users.noreply.github.com>
2026-04-21 19:56:19 -0300

    Update script.js to change image references from .JPG to .jpg and add new entries to meusJogos array.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Original content of script.js with changes applied:
+// ...your other code...
+
+// Replacing .JPG with .jpg on the specified lines
+
+let meusJogos = [
+    "FIFA15.jpg",
+    "FIFA17.jpg",
+    "THE_LAST_OF_US2.jpg",
+    "Assassins_Creed_Black.jpg",
+    // New entries added below:
+    "INFAMOUS_SECOND_SON.jpg",
+    "MINECRAFT.jpg",
+    "PES_2013.jpg",
+    "The_Last_Of_Us1.jpg",
+    "WATCH_DOGS.jpg"
+];
+// ...your other code...
